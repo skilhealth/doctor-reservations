9e9016b8880516e1b42c94ce97d2c8869de988ff
nayasuna <nayasuna01@students.amikom.ac.id>
2023-10-16 00:40:54 +0700

    memperbaiki login.js

--- js/login.js
+++ js/login.js
@@ -1,13 +1,58 @@
-function func() {
-  var username = document.getElementById("Username/Email").value;
-  var pass = document.getElementById("Password").value;
+function Login() {
+  let email = document.forms["form-login"]["email"].value.trim();
+  let password = document.forms["form-login"]["password"].value.trim();
 
-  if (username == 'skilhealth01' && pass == '12345'){
-    alert("Succesfull Login")
-    window.location.assign("home.html")
+  if (email.length == 0) {
+    alert("Email Masih Kosong!");
+    return false;
   }
-  else{
-    alert("Login Invalid")
+
+  if (password.length == 0) {
+    alert("Password Masih Kosong!");
+    return false;
   }
+
+  // FETCH DATA
+  const url = new URL(
+    "https://652c1049d0d1df5273ef1aa4.mockapi.io/registeruser"
+  );
+  url.searchParams.append("Email", email); //https://PROJECT_TOKEN.mockapi.io/tasks?completed=false
+
+  fetch(url, {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((user) => {
+      if (user.length === 0) {
+        return alert("Email atau Password Salah!");
+      } else {
+        if (user[0].Password !== password) {
+          return alert("Email atau Password Salah!");
+        } else {
+          localStorage.setItem("userData", JSON.stringify(user));
+          window.location.replace("http://127.0.0.1:7777/dashboard.html");
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
+// function func() {
+//     var username = document.getElementById("Username/Email").value;
+//     var pass = document.getElementById("Password").value;
+
+//     if (username == 'skilhealth01' && pass == '12345'){
+//       alert("Succesfull Login")
+//       window.location.assign("home.html")
+//     }
+//     else{
+//       alert("Login Invalid")
+//     }
+//   }

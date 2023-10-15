d76f1341fdc01bde206c34044d25caffaf4cd204
nayasuna <nayasuna01@students.amikom.ac.id>
2023-10-15 20:52:47 +0700

    memperbaiki login.js

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function func() {
   var username = document.getElementById("Username/Email").value;
   var pass = document.getElementById("Password").value;
 
-  if (username == 'skillhealth01' && pass == '12345'){
+  if (username == 'skilhealth01' && pass == '12345'){
     alert("Succesfull Login")
     window.location.assign("home.html")
   }

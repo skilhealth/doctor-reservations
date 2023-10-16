d2fa1961bb40e32094a098eac6348449da5fdffe
awanvirgi <virgiawan.dev15@gmail.com>
2023-10-17 02:25:31 +0700

    Dinamis Form

--- js/search-doktor.js
+++ js/search-doktor.js
@@ -6,18 +6,81 @@ let specialistValue = document.getElementById("specialist")
 let metodeValue = document.getElementById("metode")
 let form = document.getElementById("form")
 
+function cariDokter(doctors, name, day, hospital, specialist, metode) {
+    name = name || '';
+    day = day || '';
+    hospital = hospital || '';
+    specialist = specialist || '';
+    metode = metode || '';
+    let kondisi
+    return doctors.filter(doctor => {
+        kondisi = (
+            (name === '' || name === doctor.name) &&
+            (day === '' || doctor.schedule.some(schedule => schedule.day === day)) &&
+            (specialist === '' || doctor.specialist === specialist) &&
+            (hospital ==='' || doctor.hospital === hospital) &&
+            (metode ==='' || doctor.schedule.some(schedule => schedule.work === metode))
+        )
+    })
+    console.log(kondisi)
+
+
+
+}
+
+
 async function getData() {
     try {
         let result = await fetch('https://6525187f67cfb1e59ce69680.mockapi.io/doctor')
         let doctors = await result.json()
-        console.log(doctors)
+        return doctors
     }
     catch (err) {
         console("Error Fetching" + err)
     }
 }
-let getdoctor = getData()
-console.log(getdoctor)
+
+function isioptionRS(doctors) {
+    const hospitallist = [...new Set(doctors.map(doctorz => doctorz.hospital))]
+    hospitallist.forEach(element => {
+        let optionRS = `
+            <option value="${element}">${element}</option>
+            `
+        hospitalValue.innerHTML += optionRS
+
+    });
+}
+
+function isioptionSpecial(doctors) {
+    const specialists = [...new Set(doctors.map(doctorz => doctorz.specialist))]
+    specialists.forEach(element => {
+        let optionRS = `
+            <option value="${element}">${element}</option>
+            `
+        specialistValue.innerHTML += optionRS
+
+    });
+}
+
+
+
+
+
+
+// const found = cariDokter(doctors,cariValue.value,dayValue.value,hospitalValue.value,specialistValue.value,metodeValue.value)
+// console.log(found)
+getData().then(result => {
+    isioptionRS(result)
+    isioptionSpecial(result)
+
+})
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    getData().then(result => {
+        cariDokter(result, cariValue.value, dayValue.value, hospitalValue.value, specialistValue.value, metodeValue.value)
+    })
+
+})
 
 
 // form.addEventListener('submit', )

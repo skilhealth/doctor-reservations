574c2385f4a2bd2cc567e1827efc04f404768374
nayasuna <nayasuna01@students.amikom.ac.id>
2023-10-17 22:06:08 +0700

    membuat login

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+let loginstate = sessionStorage.getItem("loginState")
+const navWrap = document.getElementsByClassName("nav-content")
+
+function navbar(state){
+    if(state){
+        return `
+            <li><a href="#" onclick="clearstate()">Logout</a></li>
+        `
+    }else{
+        return `
+            <li><a href="page/login.html">Login</a></li>
+            <li><a href="page/register.html">Register</a></li>
+        `
+    }
+}
+navWrap[0].innerHTML += navbar(loginstate)
+function cleatstate(){
+    sessionStorage.setItem('loginState',JSON.stringify(false))
+}
 function backpage(){
-    window.history.back()
+    history.back();
 }

b7684f6adaaa4fc1b1704a38d2e8996258b938c4
awanvirgi <virgiawan.dev15@gmail.com>
2023-10-17 11:55:18 +0700

    Mesin pencari done

--- js/search-doktor.js
+++ js/search-doktor.js
@@ -6,26 +6,16 @@ let specialistValue = document.getElementById("specialist")
 let metodeValue = document.getElementById("metode")
 let form = document.getElementById("form")
 
-// let doktor = {
-//     day: dayValue.value,
-//     metode: metodeValue.value
-// }
+function findElementsWithCount(array, targetCount) {
+    const frequencyCounter = array.reduce((acc, curr) => {
+        acc[curr] = (acc[curr] || 0) + 1;
+        return acc;
+    }, {});
 
-// function findElementsWithCount(array, targetCount) {
-//     return Object.keys(
-//         array.reduce((acc, curr) => {
-//             acc[curr] = (acc[curr] || 0) + 1;
-//             return acc;
-//         }, {})
-//     ).filter(element => frequencyCounter[element] === targetCount);
-// }
+    const elementsWithTargetCount = Object.keys(frequencyCounter).filter(element => frequencyCounter[element] === targetCount);
 
-// // Contoh penggunaan
-// let myArray = [1, 2, 3, 4, 2, 2, 3, 1, 4, 4, 5];
-// let targetCount = 3;
-
-// let result = findElementsWithCount(myArray, targetCount);
-// console.log(`Elemen dengan jumlah kemunculan ${targetCount} adalah: ${result.join(', ')}`);
+    return elementsWithTargetCount;
+}
 
 function cariDokter(doctors, name, day, hospital, specialist, metode) {
     name = name || '';
@@ -33,16 +23,15 @@ function cariDokter(doctors, name, day, hospital, specialist, metode) {
     hospital = hospital || '';
     specialist = specialist || '';
     metode = metode || '';
-    console.log(day)
-    console.log(specialist)
+
     let doktorid = []
-    let found1 = 0,found2= 0,found3,found4,found5
+    let found1 = 0, found2 = 0, found3 = 0, found4 = 0, found5 = 0
     doctors.forEach((element) => {
-        if (specialist == element.specialist) {
+        if (specialist === element.specialist) {
             doktorid.push(element.id)
             found1 = 1;
         }
-        if (hospital == element.hospital) {
+        if (hospital === element.hospital) {
             doktorid.push(element.id)
             found2 = 1;
         }
@@ -56,51 +45,12 @@ function cariDokter(doctors, name, day, hospital, specialist, metode) {
         }
     })
     let found = found1 + found2 + found3 + found4 + found5
-    console.log(found)
     return {
-        doktorid : doktorid,
-        get : found
+        doktorid: doktorid,
+        get: found
     }
-    // console.log(doktorid)
-    // let data = doctors.filter(doctor =>{
-    //     let kondisi = (
-    //         (name === '' || name === doctor.name) &&
-    //         (day === '' || doctor.schedule.some(schedule => schedule.day === day)) &&
-    //         (specialist === '' || doctor.specialist === specialist) &&
-    //         (hospital ==='' || doctor.hospital === hospital) &&
-    //         (metode ==='' || doctor.schedule.some(schedule => schedule.work === metode))
-    //     );
-    // });  
-    // console.log(data)
+
 }
-
-// function cariDokter(doctors, criteria) {
-//     criteria = criteria || {};
-
-//     return doctors.filter(doctor => {
-//         return Object.entries(criteria).every(([key, value]) => {
-//             if (value == null) {
-//                 return true; // Melewati properti yang null atau undefined
-//             }
-//             if (key === 'day' || key === 'metode') {
-//                 return doctor.schedule.some(schedule => schedule[key] === value);
-//             }
-//             return doctor[key] === value;
-//         });
-//     });
-// }
-
-
-
-// function cariDokter(doctors, name, day, hospital, specialist, metode){
-//     let id = []
-//     doctors.forEach((element)=>{
-//         if(element.name === name){
-//             id.unshift(element.id)
-//         }
-//     })
-// }
-
 
 async function getData() {
     try {
@@ -109,7 +59,7 @@ async function getData() {
         return doctors
     }
     catch (err) {
-        console("Error Fetching" + err)
+        console.error("Error Fetching" + err)
     }
 }
 
@@ -135,13 +85,6 @@ function isioptionSpecial(doctors) {
     });
 }
 
-
-
-
-
-
-// const found = cariDokter(doctors,cariValue.value,dayValue.value,hospitalValue.value,specialistValue.value,metodeValue.value)
-// console.log(found)
 getData().then(result => {
     isioptionRS(result)
     isioptionSpecial(result)
@@ -151,10 +94,8 @@ form.addEventListener('submit', (event) => {
     event.preventDefault()
     getData().then(result => {
         let data = cariDokter(result, cariValue.value, dayValue.value, hospitalValue.value, specialistValue.value, metodeValue.value)
-        console.log(data)
-        // cariDokter(result, doktor)
+        let hasil = findElementsWithCount(data.doktorid, data.get)
+        sessionStorage.setItem(listdokter,JSON.stringify(hasil))
+
     })
 })
-
-
-// form.addEventListener('submit', )

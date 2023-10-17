aa01154564a98383f83278a4b283d11b1b49d8df
awanvirgi <virgiawan.dev15@gmail.com>
2023-10-17 08:14:45 +0700

    Mesin Cari

--- js/search-doktor.js
+++ js/search-doktor.js
@@ -6,27 +6,100 @@ let specialistValue = document.getElementById("specialist")
 let metodeValue = document.getElementById("metode")
 let form = document.getElementById("form")
 
+// let doktor = {
+//     day: dayValue.value,
+//     metode: metodeValue.value
+// }
+
+// function findElementsWithCount(array, targetCount) {
+//     return Object.keys(
+//         array.reduce((acc, curr) => {
+//             acc[curr] = (acc[curr] || 0) + 1;
+//             return acc;
+//         }, {})
+//     ).filter(element => frequencyCounter[element] === targetCount);
+// }
+
+// // Contoh penggunaan
+// let myArray = [1, 2, 3, 4, 2, 2, 3, 1, 4, 4, 5];
+// let targetCount = 3;
+
+// let result = findElementsWithCount(myArray, targetCount);
+// console.log(`Elemen dengan jumlah kemunculan ${targetCount} adalah: ${result.join(', ')}`);
+
 function cariDokter(doctors, name, day, hospital, specialist, metode) {
     name = name || '';
     day = day || '';
     hospital = hospital || '';
     specialist = specialist || '';
     metode = metode || '';
-    let kondisi
-    return doctors.filter(doctor => {
-        kondisi = (
-            (name === '' || name === doctor.name) &&
-            (day === '' || doctor.schedule.some(schedule => schedule.day === day)) &&
-            (specialist === '' || doctor.specialist === specialist) &&
-            (hospital ==='' || doctor.hospital === hospital) &&
-            (metode ==='' || doctor.schedule.some(schedule => schedule.work === metode))
-        )
+    console.log(day)
+    console.log(specialist)
+    let doktorid = []
+    let found1 = 0,found2= 0,found3,found4,found5
+    doctors.forEach((element) => {
+        if (specialist == element.specialist) {
+            doktorid.push(element.id)
+            found1 = 1;
+        }
+        if (hospital == element.hospital) {
+            doktorid.push(element.id)
+            found2 = 1;
+        }
+        if (element.schedule.some(schedule => schedule.day === day)) {
+            doktorid.push(element.id)
+            found3 = 1;
+        }
+        if (element.schedule.some(schedule => schedule.work === metode)) {
+            doktorid.push(element.id)
+            found4 = 1;
+        }
     })
-    console.log(kondisi)
-
-
-
+    let found = found1 + found2 + found3 + found4 + found5
+    console.log(found)
+    return {
+        doktorid : doktorid,
+        get : found
+    }
+    // console.log(doktorid)
+    // let data = doctors.filter(doctor =>{
+    //     let kondisi = (
+    //         (name === '' || name === doctor.name) &&
+    //         (day === '' || doctor.schedule.some(schedule => schedule.day === day)) &&
+    //         (specialist === '' || doctor.specialist === specialist) &&
+    //         (hospital ==='' || doctor.hospital === hospital) &&
+    //         (metode ==='' || doctor.schedule.some(schedule => schedule.work === metode))
+    //     );
+    // });  
+    // console.log(data)
 }
+
+// function cariDokter(doctors, criteria) {
+//     criteria = criteria || {};
+
+//     return doctors.filter(doctor => {
+//         return Object.entries(criteria).every(([key, value]) => {
+//             if (value == null) {
+//                 return true; // Melewati properti yang null atau undefined
+//             }
+//             if (key === 'day' || key === 'metode') {
+//                 return doctor.schedule.some(schedule => schedule[key] === value);
+//             }
+//             return doctor[key] === value;
+//         });
+//     });
+// }
+
+
+
+// function cariDokter(doctors, name, day, hospital, specialist, metode){
+//     let id = []
+//     doctors.forEach((element)=>{
+//         if(element.name === name){
+//             id.unshift(element.id)
+//         }
+//     })
+// }
 
 
 async function getData() {
@@ -77,9 +150,10 @@ getData().then(result => {
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     getData().then(result => {
-        cariDokter(result, cariValue.value, dayValue.value, hospitalValue.value, specialistValue.value, metodeValue.value)
+        let data = cariDokter(result, cariValue.value, dayValue.value, hospitalValue.value, specialistValue.value, metodeValue.value)
+        console.log(data)
+        // cariDokter(result, doktor)
     })
-
 })
 
 
